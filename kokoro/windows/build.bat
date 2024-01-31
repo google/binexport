5795afc727e7ab66072ea12b38f3e9c978bfa046
Christian Blichmann <cblichmann@google.com>
2024-01-31 05:39:08 -0800

    Windows: Move code signing to internal build chain
    PiperOrigin-RevId: 603021088
    Change-Id: I3de84278503578d6d218e38dda1ae54822769b05

--- kokoro/windows/build.bat
+++ kokoro/windows/build.bat
@@ -6,22 +6,14 @@ if "%1" == "release" (
 
 echo on
 
-set SIGNTOOL="%ProgramFiles(x86)%\Windows kits\10\bin\x86\signtool.exe"
-
 set BUILD_DIR=%cd%\build
 if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
 
-:: Hermetic VS2022 (Community) toolchain using CMake and Ninja
 unzip -q "%KOKORO_GFILE_DIR%\cmake-3.25.2-windows-x86_64.zip" -d "%BUILD_DIR%" || exit /b
 unzip -q "%KOKORO_GFILE_DIR%\ninja-win.zip" -d "%BUILD_DIR%" || exit /b
-unzip -q "%KOKORO_GFILE_DIR%\vs2022.zip" -d "%BUILD_DIR%" || exit /b
+
 set CMAKE_BIN=%BUILD_DIR%\cmake-3.25.2-windows-x86_64\bin
-set VS_PATH=%BUILD_DIR%\vs2022\14.33.31629
-set SDK_PATH=%ProgramFiles(x86)%\Windows Kits\10
-set SDK_VER=10.0.16299.0
-set PATH=%SystemRoot%\system32;%VS_PATH%\bin\HostX64\x64;%SDK_PATH%\bin\%SDK_VER%\x64;%BUILD_DIR%;%CMAKE_BIN%
-set LIB=%VS_PATH%\lib\x64;%SDK_PATH%\Lib\%SDK_VER%\um\x64;%SDK_PATH%\Lib\%SDK_VER%\ucrt\x64;
-set INCLUDE=%VS_PATH%\include;%SDK_PATH%\Include\%SDK_VER%\ucrt;%SDK_PATH%\Include\%SDK_VER%\um;%SDK_PATH%\Include\%SDK_VER%\shared;%SDK_PATH%\Include\%SDK_VER%\winrt;%SDK_PATH%\Include\%SDK_VER%\cppwinrt
+set PATH=%PATH%;%CMAKE_BIN%
 
 :: Build BinExport
 set SRC_DIR=%KOKORO_ARTIFACTS_DIR%/git
@@ -51,18 +43,3 @@ ctest --build-config Release --output-on-failure -R "^[A-Z]" || exit /b
 cmake --install . --config Release --strip || exit /b
 popd
 
-if "%1" neq "release" exit /b %ERRORLEVEL%
-
-:: Release build, code sign the artifacts
-echo Code signing artifacts...
-
-set ARTIFACTS=^
-  "%OUT_DIR%\binexport-prefix\binexport*.dll" ^
-  "%OUT_DIR%\binexport-prefix\binexport2dump.exe"
-
-%SIGNTOOL% sign /v /tr http://timestamp.digicert.com /n "Google" /a /fd sha256 ^
-  /td sha256 %ARTIFACTS%
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-
-%SIGNTOOL% verify /pa /all %ARTIFACTS%
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
