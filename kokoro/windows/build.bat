bda81ecd7f41107d53fa8748dd7244b3c0532946
Christian Blichmann <cblichmann@google.com>
2024-05-10 05:44:31 -0700

    Update internal build script
    PiperOrigin-RevId: 632468155
    Change-Id: Iafab5728580512fa4d39f7d490f51431c3f7ab7b

--- kokoro/windows/build.bat
+++ kokoro/windows/build.bat
@@ -9,12 +9,6 @@ echo on
 set BUILD_DIR=%cd%\build
 if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
 
-unzip -q "%KOKORO_GFILE_DIR%\cmake-3.25.2-windows-x86_64.zip" -d "%BUILD_DIR%" || exit /b
-unzip -q "%KOKORO_GFILE_DIR%\ninja-win.zip" -d "%BUILD_DIR%" || exit /b
-
-set CMAKE_BIN=%BUILD_DIR%\cmake-3.25.2-windows-x86_64\bin
-set PATH=%PATH%;%CMAKE_BIN%
-
 :: Build BinExport
 set SRC_DIR=%KOKORO_ARTIFACTS_DIR%/git
 set OUT_DIR=%BUILD_DIR%
@@ -25,8 +19,13 @@ xcopy /q /s /e ^
   "%KOKORO_PIPER_DIR%\google3\third_party\jsoncpp" ^
   "%KOKORO_PIPER_DIR%\google3\third_party\binaryninja_api\third_party\jsoncpp\"
 
+:: Help debug build issues
+set
+ninja --help
+
 pushd "%OUT_DIR%"
 cmake "%SRC_DIR%/binexport" ^
+  --debug-output --debug-trycompile ^
   -G "Ninja" ^
   -DFETCHCONTENT_FULLY_DISCONNECTED=ON ^
   "-DFETCHCONTENT_SOURCE_DIR_ABSL=%KOKORO_ARTIFACTS_DIR%\git\absl" ^
